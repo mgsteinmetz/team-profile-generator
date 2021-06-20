177ee76ba9bd7252cfd38161585711574032c9f5
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-06-20 16:29:31 -0500

    finishing prompts

--- index.js
+++ index.js
@@ -8,31 +8,55 @@ const generateHTML = (answers) =>
 
 inquirer
 .prompt([
-{
-    type: 'input',
-    name: 'name',
-    message: 'Who are you wanting to add?'
-},
-{
-    type: 'input',
-    name: 'job',
-    message: 'What is their job title?'
-},
-{
-    type: 'input',
-    name: 'ID',
-    message: 'What is thier ID number?'
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'What is their primary email?'
-},
-{
-    type: 'input',
-    name: 'name',
-    message: 'What is their GitHub account?'
-},
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the persons name?'
+    },
+    {
+        type: 'checkbox',
+        name: 'job',
+        message: 'What is their job title?',
+        choices: ['Manager', 'Engineer', 'Employee', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is thier ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their primary email?'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is their office number?',
+        when: (data) => data.role === 'Manager'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is their GitHub account?',
+        when: (data) => data.role === 'Employee'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Where are they going to school?',
+        when: (data) => data.role === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their primary email?'
+    },
+    {
+        type: 'confirm',
+        name: 'newPerson',
+        message: 'Would you like to add another person?'
+    },
 ])
 .then((answers) => {
     const htmlPageContent = generateHTML(answers);
