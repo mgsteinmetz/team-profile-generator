4b1f41732f46092d96c3f40a8800b7d8b2dcd265
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-06-21 12:34:00 -0500

    inputing sample html into index.js

--- index.js
+++ index.js
@@ -2,9 +2,67 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateHTML = (answers) => 
-`
-
-`;
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/5.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Profile</title>
+</head>
+<body>
+    <header class="jumbotron text-center bg-dark" style="color: whitesmoke;">
+        <h1><bold>My Team</bold></h1>
+    </header>
+    <main>
+        <div class="card-group">
+            <div class="card container m-3" style="width: 18rem;">
+                <div class="card-header bg-primary row" style="color: whitesmoke; height: 100px;">
+                    <h3>${answers.name} ${answers.job}</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${answers.id}</li>
+                    <li class="list-group-item">Email: ${answers.email}</li>
+                    <li class="list-group-item">Office Number: ${answers.officeNumber}</li>
+                </ul>
+            </div>
+            <div class="card container m-3" style="width: 18rem;">
+                <div class="card-header bg-primary row" style="color: whitesmoke; height: 100px;">
+                    <h3>${answers.name} ${answers.job}</h3>
+                </div>                
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${answers.id}</li>
+                    <li class="list-group-item">Email: ${answers.email}</li>
+                    <li class="list-group-item">GitHub: ${answers.github}</li>
+                </ul>
+            </div>
+            <div class="card container m-3" style="width: 18rem;">
+                <div class="card-header bg-primary row" style="color: whitesmoke; height: 100px;">
+                    <h3>${answers.name} ${answers.job}</h3>
+                </div>                
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${answers.id}</li>
+                        <li class="list-group-item">Email: ${answers.email}</li>
+                        <li class="list-group-item">GitHub: ${answers.github}</li>
+                    </ul>
+            </div>
+            <div class="card container m-3" style="width: 18rem;">
+                <div class="card-header bg-primary row" style="color: whitesmoke; height: 100px;">
+                    <h3>${answers.name} ${answers.job}</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${answers.id}</li>
+                    <li class="list-group-item">Email: ${answers.email}</li>
+                    <li class="list-group-item">School: ${answers.school}</li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+</html>`;
 
 inquirer
 .prompt([
@@ -39,7 +97,7 @@ inquirer
         type: 'input',
         name: 'github',
         message: 'What is their GitHub account?',
-        when: (data) => data.role === 'Employee'
+        when: (data) => data.role === 'Engineer'
     },
     {
         type: 'input',
